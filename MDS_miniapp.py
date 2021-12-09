31c15043bd46e3db5d34be38810424b694f86320
palsubhadeep1234 <95303092+palsubhadeep1234@users.noreply.github.com>
2021-12-09 08:36:27 -0600

    Update MDS_miniapp.py

--- MDS_miniapp.py
+++ MDS_miniapp.py
@@ -107,6 +107,8 @@ if no_prediction == 1:
     st.markdown(f"""The player can play in __*{pos[index[0]]}*__ position""")
 elif no_prediction == 0:
     st.markdown(f"""There is no distinct player position for given statistics""")
+elif  np.count_nonzero(test_data == 0) == 4:
+    st.markdown(f"""PLease enter player statistics""")
 else:
     for i in range(0,5):
         if y_pred_process[0,i] == 1:
